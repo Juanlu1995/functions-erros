086c1913bce29124e2b3bc8955b7b28bc788353c
Juan Luis Romero <juanlu_21@hotmail.com>
2017-10-11 21:26:19 +0200

    añadidos

--- errores&funciones.js
+++ errores&funciones.js
@@ -93,3 +93,68 @@ function sumadoresAnonimos(){
     console.log(suma5(2));
     console.log(suma10(2));
 }
+
+function creaDividor(x) {
+    return function(y) {
+        return y / x;
+    };
+}
+let creaDivir2 = creaDividor(2);
+console.log(creaDivir2(10));
+
+
+function comparatorNumber(x,y) {
+    if (x === y) return 0;
+    if (x < y) return -1;
+    if (x > y) return 1;
+}
+console.log(comparatorNumber(10,10));
+console.log(comparatorNumber(9,10));
+console.log(comparatorNumber(11,10));
+
+function numeroMayorCadena(x) {
+    return function () {
+        let numeros = [];
+        let masAlto = 0;
+        let cadena = x.split(" ");
+        for(let i = 0 ; i < cadena.length ; i++) {
+            if (!isNaN(cadena[i])){
+                numeros.push(parseInt(cadena[i]));
+            }
+        }
+        for(let i = 0 ; i < numeros.length ; i++) {
+            if (masAlto < numeros[i]){
+                masAlto = numeros[i];
+            }
+        }
+        return masAlto;
+    }
+}
+let impreso = "24 -145 15 es 5 23 mayor que 14";
+
+let funcionImpresa = x => numeroMayorCadena(x);
+console.log(funcionImpresa(impreso)());
+
+function sumaDos(x) {
+    return function (y) {
+        return x+y;
+    }
+
+}
+
+let res= z => sumaDos(z);
+
+console.log(res(3)(3));
+
+function concatenar(x) {
+    return function (y) {
+        return function (a) {
+            return function (b) {
+                return x+y+a+b;
+            }
+        }
+    }
+}
+let cadena1 = x => concatenar("hola");
+cadena1();
+
